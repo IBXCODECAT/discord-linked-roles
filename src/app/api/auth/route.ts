d0589455ddfa85ba1e7b735d38b12c63e372fd90
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2024-01-22 10:27:23 -0600

    route

---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(request: NextApiRequest) {
+    return NextResponse.json({ message: "Hello World" });
+}
